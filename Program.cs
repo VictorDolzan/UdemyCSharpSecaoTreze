c3ee944813ce51e7b2e7e2a2304813fe15dc4291
Victor Dolsan <victorddf5@gmail.com>
2022-01-02 18:51:48 -0300

    Inicializando o repositorio da secao 13 do curso de C# da Udemy- Trabalhando com arquivos

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace CSharpSecaoTreze
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
